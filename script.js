16932394b524b7e8e0b2a19de180a6b171a057f1
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-06-23 20:06:14 +0200

    events, edit playRound function, add helper functions

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const ROCK = 1;
-const PAPER = 2;
-const SCISSORS = 3;
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
 
 const playButton = document.querySelector('.playButton');
 const gameInterface = document.querySelector('.gameInterface');
+
+const playerItems = document.querySelectorAll('#playerSelection > .itemContainer');
+
+const scoreText = document.querySelector('#scoreValue');
 
 let computerWins = 0;
 let playerWins = 0;
@@ -14,36 +18,55 @@ playButton.addEventListener('click', () => {
     console.log('Removed play button and showed game interface');
 })
 
-function initGame() {
+playerItems.forEach((selectionDiv, selectionType) => {
+    selectionDiv.addEventListener('click', () => {
+        playRound(selectionType);
+    })
+});
 
-}
+function playRound(playerSelection) {
+    activateGameItems(false);
 
-function game() {
-    let gameContinue = true;
-
-    while (gameContinue) {
-        // czekaj na input gracza
-        // wylosuj odpowiedz komputera
-        playRound();
-
-        if (computerWins >= 5 || playerWins >= 5) gameContinue = false;
-    }
-}
-
-function playRound() {
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
+    const playerMove = playerSelection;
+    const computerMove = computerPlay();
     
+    /*console.log(`Playing round, player chose ${playerSelection}, computer
+        chose ${computerMove}`);*/
+
+    animateComputerSelection(computerMove);
+
+    // przedstaw i zaktualizuj wynik
+    computeResult(playerMove, computerMove);
+    updateResult();
+
+    activateGameItems(true);
+}
+
+function activateGameItems(mode) {
+    playerItems.forEach((item) => {
+        if (mode)  item.style.pointerEvents = 'auto';
+        if (!mode) item.style.pointerEvents = 'none';
+    });
 }
 
 function computerPlay() {
     return Math.floor(Math.random() * 3);
 }
 
-function playerPlay() {
-    while (true) {
-        if ()
-    }
+function animateComputerSelection(computerMove) {
+    // zrob animacje
+    // na koncu podswietl div-a, ktorego wybral komputer
+}
+
+function computeResult(plaMove, comMove) {
+    if (plaMove === comMove) return;
+    if (plaMove - comMove === 1 ||
+        plaMove - comMove === -2) playerWins++;
+    else computerWins++;
+}
+
+function updateResult() {
+    scoreText.textContent = `Player ${playerWins} | ${computerWins} Computer`;
 }
 
 function computeFinalResult() {
